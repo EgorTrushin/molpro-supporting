3c9efa4aed0ff8e683d4179278e80c974feaefb1
Egor Trushin <trushin@tccd107.chemie.uni-erlangen.de>
2023-04-05 13:24:50 +0200

    scexx testjobs chaged and script improved

--- testjobs/run_testjobs.sh
+++ testjobs/run_testjobs.sh
@@ -2,15 +2,15 @@
 
 rm *.out *.xml *.log
 
-MOLPRO=/home/trushin/Molpro/molpro-master/bin/molpro
-NUM_OMP_THREADS=16
+MOLPRO=/home/trushin/Molpro/molpro-fork/bin/molpro
+NUM_OMP_THREADS=28
 
 echo Molpro Path: $MOLPRO
 echo Number of OMP threads: $NUM_OMP_THREADS
 
 export OMP_THREAD_LIMIT=$NUM_OMP_THREADS
 
-$MOLPRO -t $NUM_OMP_THREADS < co_acfd_rirpa.test
-$MOLPRO -t $NUM_OMP_THREADS < co_acfd_scexx.test
-$MOLPRO -t $NUM_OMP_THREADS < nh2_acfd_urirpa.test
-$MOLPRO -t $NUM_OMP_THREADS < nh2_acfd_uscexx.test 
+$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < co_acfd_rirpa.test
+$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < co_acfd_scexx.test
+$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < nh2_acfd_urirpa.test
+$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < nh2_acfd_uscexx.test 
