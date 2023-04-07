6a148e8ebe21297dd8c83fe5db72a2e8bad418f4
Egor Trushin <e.v.trushin@gmail.com>
2023-04-07 23:58:42 +0200

    testjob for KSINV is added

--- testjobs/run_testjobs.sh
+++ testjobs/run_testjobs.sh
@@ -3,7 +3,7 @@
 rm *.out *.xml *.log
 
 MOLPRO=/home/trushin/Molpro/molpro-fork/bin/molpro
-NUM_OMP_THREADS=28
+NUM_OMP_THREADS=16
 
 echo Molpro Path: $MOLPRO
 echo Number of OMP threads: $NUM_OMP_THREADS
@@ -12,5 +12,6 @@ export OMP_THREAD_LIMIT=$NUM_OMP_THREADS
 
 $MOLPRO -t $NUM_OMP_THREADS --no-xml-output < co_acfd_rirpa.test
 $MOLPRO -t $NUM_OMP_THREADS --no-xml-output < co_acfd_scexx.test
+$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < co_ksinv.test
 $MOLPRO -t $NUM_OMP_THREADS --no-xml-output < nh2_acfd_urirpa.test
-$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < nh2_acfd_uscexx.test 
+$MOLPRO -t $NUM_OMP_THREADS --no-xml-output < nh2_acfd_uscexx.test
