c76d7fc519a2f9ffe0170c644773f380b31b77ab
Egor Trushin <e.v.trushin@gmail.com>
2025-07-25 20:16:42 +0200

    improve

--- testjobs/run_testjobs.sh
+++ testjobs/run_testjobs.sh
@@ -2,22 +2,22 @@
 
 rm *.out *.xml *.log
 
-MOLPRO=/home/trushin/Molpro/molpro/bin/molpro.exe
-NUM_OMP_THREADS=16
+MOLPRO=$HOME/Molpro/molpro/bin/molpro
+OMP_NUM_THREADS=16
 MEM=2000
 
 echo Molpro Path: $MOLPRO
-echo Number of OMP threads: $NUM_OMP_THREADS
+echo Number of OMP threads: $OMP_NUM_THREADS
 echo Memory: $MEM m
 
-export OMP_NUM_THREADS=$NUM_OMP_THREADS
+export OMP_NUM_THREADS=$OMP_NUM_THREADS
 
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < b_uksinv.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < be_acfd_scrpa.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < co_acfd_rirpa.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < co_acfd_scexx.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < co_ksinv.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < li_acfd_uscrpa.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < li_ksinv.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < nh2_acfd_urirpa.test
-mpiexec -n 1 $MOLPRO -m $MEM --no-xml-output < nh2_acfd_uscexx.test
+$MOLPRO -m $MEM --no-xml-output < b_uksinv.test
+$MOLPRO -m $MEM --no-xml-output < be_acfd_scrpa.test
+$MOLPRO -m $MEM --no-xml-output < co_acfd_rirpa.test
+$MOLPRO -m $MEM --no-xml-output < co_acfd_scexx.test
+$MOLPRO -m $MEM --no-xml-output < co_ksinv.test
+$MOLPRO -m $MEM --no-xml-output < li_acfd_uscrpa.test
+$MOLPRO -m $MEM --no-xml-output < li_ksinv.test
+$MOLPRO -m $MEM --no-xml-output < nh2_acfd_urirpa.test
+$MOLPRO -m $MEM --no-xml-output < nh2_acfd_uscexx.test
