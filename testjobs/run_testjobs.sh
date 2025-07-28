2d9108719362f03fde4cd90ad137f86565c4cfa1
Egor Trushin <e.v.trushin@gmail.com>
2025-07-28 20:39:44 +0200

    dftoep testjobs

--- testjobs/run_testjobs.sh
+++ testjobs/run_testjobs.sh
@@ -14,10 +14,12 @@ export OMP_NUM_THREADS=$OMP_NUM_THREADS
 
 $MOLPRO -m $MEM --no-xml-output < b_uksinv.test
 $MOLPRO -m $MEM --no-xml-output < be_acfd_scrpa.test
+$MOLPRO -m $MEM --no-xml-output < co_acfd_dftoep.test
 $MOLPRO -m $MEM --no-xml-output < co_acfd_rirpa.test
 $MOLPRO -m $MEM --no-xml-output < co_acfd_scexx.test
 $MOLPRO -m $MEM --no-xml-output < co_ksinv.test
 $MOLPRO -m $MEM --no-xml-output < li_acfd_uscrpa.test
 $MOLPRO -m $MEM --no-xml-output < li_ksinv.test
+$MOLPRO -m $MEM --no-xml-output < n_acfd_udftoep.test
 $MOLPRO -m $MEM --no-xml-output < nh2_acfd_urirpa.test
 $MOLPRO -m $MEM --no-xml-output < nh2_acfd_uscexx.test
